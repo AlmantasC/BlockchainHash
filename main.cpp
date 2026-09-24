51078e55684565f383e112789c7681e51e37d2bd
AlmantasC <almantas.cincikas@mif.stud.vu.lt>
2026-09-24 20:52:31 +0300

    Updated hash algorithm

--- main.cpp
+++ main.cpp
@@ -9,15 +9,18 @@ int main()
     SetConsoleCP(65001);
 
     std::string in;
-    std::cin>>in;
-    uint64_t out1=0, out2=0, out3=0, out4=0;
-    int x=in.size();
-    for (int i=0; i<x; i++) {
-        out1+=in[i]*rand();
-        out2+=in[i]*rand();
-        out3+=in[i]*rand();
-        out4+=in[i]*rand();
-    }
-    std::cout<<std::hex<<std::setw(16)<<std::setfill('0')<<out1<<std::setw(16)<<std::setfill('0')<<out2<<std::setw(16)<<std::setfill('0')<<out3<<std::setw(16)<<std::setfill('0')<<out4;
-    return 0;
+    std::getline(std::cin, in);
+
+    uint64_t a[4] = { 1, 2, 3, 4 };
+    uint64_t b[4] = {
+        0x9e3779b97f4a7c15ULL, 0xbf58476d1ce4e5b9ULL,
+        0x94d049bb133111ebULL, 0x100000001b3ULL
+    };
+    for (unsigned char c : in)
+        for (int i=0; i<4; ++i)
+            a[i]=a[i]*b[i]+c;
+
+    std::ostringstream os;
+    for (uint64_t v : a) os<<std::hex<<std::setw(16)<<std::setfill('0')<<v;
+    std::cout<<os.str();
 }
