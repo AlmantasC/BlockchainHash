46c4d42c5316e6760b5333046c13dff47a8a4327
AlmantasC <almantas.cincikas@mif.stud.vu.lt>
2026-09-24 20:41:11 +0300

    Made very bad hash algo

--- main.cpp
+++ main.cpp
@@ -1,9 +1,23 @@
 #include <iostream>
-
-using namespace std;
-
+#include <string>
+#include <cstdint>
+#include <windows.h>
+#include <iomanip>
 int main()
 {
-    cout << "Hello world!" << endl;
+    SetConsoleOutputCP(65001);
+    SetConsoleCP(65001);
+
+    std::string in;
+    std::cin>>in;
+    uint64_t out1=0, out2=0, out3=0, out4=0;
+    int x=in.size();
+    for (int i=0; i<x; i++) {
+        out1+=in[i]*rand();
+        out2+=in[i]*rand();
+        out3+=in[i]*rand();
+        out4+=in[i]*rand();
+    }
+    std::cout<<std::hex<<std::setw(16)<<std::setfill('0')<<out1<<std::setw(16)<<std::setfill('0')<<out2<<std::setw(16)<<std::setfill('0')<<out3<<std::setw(16)<<std::setfill('0')<<out4;
     return 0;
 }
